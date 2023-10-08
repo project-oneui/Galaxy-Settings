1c29deacf544debfa068253db3519f7b335a5e91
キャッツウィズライス <103961848+Vaneeyo@users.noreply.github.com>
2023-10-08 14:32:15 +0200

    some little changes

--- app/js/main.js
+++ app/js/main.js
@@ -70,6 +70,7 @@ function openModal() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('version').innerHTML = `Version ${packageJson.version}`
     openSubMenu('samsung-widgets')
     const backdrop = document.getElementById('backdrop');
     const infoModal = document.getElementById('info-modal');
